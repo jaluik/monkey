bdf1b6441e02351d1a76f4d2ff0d67848b1b6437
jaluik <cqzhengmouren@gmail.com>
2024-08-06 22:05:14 +0800

    feat: ast and parser

---
+++ ast/ast.go
@@ -0,0 +1,53 @@
+package ast
+
+import "jaluik.com/monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+// 语句
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// 表达式，可以return的为表达式
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
