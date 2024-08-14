27776273e456f1a85af436e86b782e2f2ab14e1b
jaluik <cqzhengmouren@gmail.com>
2024-08-14 23:22:22 +0800

    feat: update let and return expression parser

--- parser/parser.go
+++ parser/parser.go
@@ -127,8 +127,10 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
-	// TODO: We're skipping the expressions until we  encouter a semicolon
-	for !p.curTokenIs(token.SEMICOLON) {
+	p.nextToken()
+	stmt.Value = p.parseExpression(LOWEST)
+
+	for p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 	return stmt
@@ -138,7 +140,10 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	stmt := &ast.ReturnStatement{Token: p.curToken}
 	p.nextToken()
-	for !p.curTokenIs(token.SEMICOLON) {
+
+	stmt.ReturnValue = p.parseExpression(LOWEST)
+
+	for !p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 	return stmt
