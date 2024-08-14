dc31c7a95c8eee757d9d8d93d50ff4b82743fe73
jaluik <cqzhengmouren@gmail.com>
2024-08-14 22:51:25 +0800

    feat: call expression

--- ast/ast.go
+++ ast/ast.go
@@ -282,3 +282,32 @@ func (f *FunctionLiteral) String() string {
 }
 
 func (f *FunctionLiteral) expressionNode() {}
+
+type CallExpression struct {
+	Token     token.Token
+	Function  Expression //标识符或函数字面量
+	Arguments []Expression
+}
+
+func (c *CallExpression) TokenLiteral() string {
+	return c.Token.Literal
+}
+
+func (c *CallExpression) String() string {
+	var out bytes.Buffer
+
+	var args []string
+	for _, a := range c.Arguments {
+		args = append(args, a.String())
+	}
+
+	out.WriteString(c.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ","))
+	out.WriteString(")")
+
+	return out.String()
+
+}
+
+func (c *CallExpression) expressionNode() {}
