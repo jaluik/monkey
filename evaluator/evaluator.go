4fcaf5a751d608a8db183fcc828bfc37c3b160ef
jaluik <cqzhengmouren@gmail.com>
2024-08-17 15:50:08 +0800

    feat: add boolean evaluated process

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"jaluik.com/monkey/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
@@ -15,7 +21,10 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{
 			Value: node.Value,
 		}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
+
 	return nil
 }
 
@@ -25,4 +34,11 @@ func evalStatements(statements []ast.Statement) object.Object {
 		result = Eval(stmt)
 	}
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
