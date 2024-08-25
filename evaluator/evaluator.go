b765f507f468c24afc50a9d0d8510875d0a94fbd
jaluik <cqzhengmouren@gmail.com>
2024-08-25 21:12:24 +0800

    fix: array evaluator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -67,6 +67,16 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return val
 		}
 		env.Set(node.Name.Value, val)
+	case *ast.IndexExpression:
+		left := Eval(node.Left, env)
+		if isError(left) {
+			return left
+		}
+		index := Eval(node.Index, env)
+		if isError(index) {
+			return index
+		}
+		return evalIndexExpression(left, index)
 	case *ast.CallExpression:
 		function := Eval(node.Function, env)
 		if isError(function) {
@@ -115,6 +125,26 @@ func extendFunction(function *object.Function, args []object.Object) *object.Env
 		env.Set(param.Value, args[paramIdx])
 	}
 	return env
+}
+
+func evalIndexExpression(left object.Object, index object.Object) object.Object {
+
+	switch {
+	case left.Type() == object.ARRAY_OBJ && (index.Type() == object.INTEGER_OBJ):
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(left object.Object, index object.Object) object.Object {
+	arrayObj := left.(*object.Array)
+	idx := index.(*object.Integer).Value
+	m := int64(len(arrayObj.Elements) - 1)
+	if idx < 0 || idx > m {
+		return NULL
+	}
+	return arrayObj.Elements[idx]
 }
 
 func evalExpressions(arguments []ast.Expression, env *object.Environment) []object.Object {
