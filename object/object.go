5d99677bd9c23cf4bdd7b8a5bddc4fae838199a8
jaluik <cqzhengmouren@gmail.com>
2024-08-26 17:55:45 +0800

    feat: parse hash literal

--- object/object.go
+++ object/object.go
@@ -3,6 +3,7 @@ package object
 import (
 	"bytes"
 	"fmt"
+	"hash/fnv"
 	"jaluik.com/monkey/ast"
 	"strings"
 )
@@ -156,4 +157,32 @@ func (a *Array) Inspect() string {
 	out.WriteString(strings.Join(elements, ","))
 	out.WriteString("]")
 	return out.String()
+}
+
+type HashKey struct {
+	Type  ObjectType
+	Value uint64
+}
+
+func (b *Boolean) HashKey() HashKey {
+	var value uint64
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+	return HashKey{Type: b.Type(), Value: value}
+}
+
+func (i *Integer) HashKey() HashKey {
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+
+func (s *String) HashKey() HashKey {
+	h := fnv.New64a()
+	_, _ = h.Write([]byte(s.Value))
+	return HashKey{
+		Type:  s.Type(),
+		Value: h.Sum64(),
+	}
 }
