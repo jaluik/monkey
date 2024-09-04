1a42aac69674f35946e2bb61d0f85d4756b17671
jaluik <cqzhengmouren@gmail.com>
2024-09-04 12:04:20 +0800

    feat: macro keyword

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -28,6 +28,7 @@ if(5<10){
 "foo bar"
 [1,2];
 {"foo": "bar"}
+macro(x,y) {x + y;};
 `
 
 	tests := []struct {
@@ -119,6 +120,19 @@ if(5<10){
 		{token.COLON, ":"},
 		{token.STRING, "bar"},
 		{token.RBRACE, "}"},
+		{token.MACRO, "macro"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.INT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 	l := New(input)
--- token/token.go
+++ token/token.go
@@ -48,6 +48,8 @@ const (
 	IF       = "IF"
 	ELSE     = "ELSE"
 	RETURN   = "RETURN"
+
+	MACRO = "MACRO"
 )
 
 var keywords = map[string]TokenType{
@@ -58,6 +60,7 @@ var keywords = map[string]TokenType{
 	"if":     IF,
 	"else":   ELSE,
 	"return": RETURN,
+	"macro":  MACRO,
 }
 
 func LookupIdent(ident string) TokenType {
