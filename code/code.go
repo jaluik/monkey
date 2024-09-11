9591f418a38b1cdcd1f84aee9c3babc2aaa7bba0
jaluik <cqzhengmouren@gmail.com>
2024-09-11 21:20:18 +0800

    feat: code definition

--- code/code.go
+++ code/code.go
@@ -1,5 +1,7 @@
 package code
 
+import "fmt"
+
 type Instructions []byte
 
 type Opcode byte
@@ -7,3 +9,20 @@ type Opcode byte
 const (
 	OpConstant Opcode = iota
 )
+
+type Definition struct {
+	Name          string
+	OperandWidths []int
+}
+
+var definitions = map[Opcode]*Definition{
+	OpConstant: {"OpConstant", []int{2}},
+}
+
+func Lookup(op byte) (*Definition, error) {
+	def, ok := definitions[Opcode(op)]
+	if !ok {
+		return nil, fmt.Errorf("opcode %d undefined", op)
+	}
+	return def, nil
+}
