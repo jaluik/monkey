79458fc1bd150ec4a52c0c3d6d10b8a2af5aa12f
jaluik <cqzhengmouren@gmail.com>
2024-09-13 12:16:50 +0800

    feat: compiler

---
+++ compiler/compiler.go
@@ -0,0 +1,34 @@
+package compiler
+
+import (
+	"jaluik.com/monkey/ast"
+	"jaluik.com/monkey/code"
+	"jaluik.com/monkey/object"
+)
+
+type Compiler struct {
+	instructions code.Instructions
+	constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+func (c *Compiler) Compile(node ast.Node) error {
+	return nil
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instructions: c.instructions,
+		Constants:    c.constants,
+	}
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
