7f3d7948d0a32fcd926afe40fb296b84f7807b95
jaluik <cqzhengmouren@gmail.com>
2024-09-17 23:03:58 +0800

    feat: compiler constant

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -18,6 +18,35 @@ func New() *Compiler {
 	}
 }
 func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{
+			Value: node.Value,
+		}
+		c.emit(code.OpConstant, c.addConstant(integer))
+
+	}
 	return nil
 }
 
@@ -26,6 +55,23 @@ func (c *Compiler) Bytecode() *Bytecode {
 		Instructions: c.instructions,
 		Constants:    c.constants,
 	}
+}
+
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+func (c *Compiler) emit(op code.Opcode, operands ...int) int {
+	ins := code.Make(op, operands...)
+	pos := c.addInstructions(ins)
+	return pos
+}
+
+func (c *Compiler) addInstructions(ins []byte) int {
+	posNerInstruction := len(c.instructions)
+	c.instructions = append(c.instructions, ins...)
+	return posNerInstruction
 }
 
 type Bytecode struct {
